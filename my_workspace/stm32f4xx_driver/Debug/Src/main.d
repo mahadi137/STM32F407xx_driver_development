7e6473a3fb358078ec59c4a1bc10f4ccee274c10
Sayad Mohammad Mahadi Hassan <71854514+mahadi137@users.noreply.github.com>
2025-12-23 00:03:13 +0200

    new repository for MCU1

---
+++ my_workspace/stm32f4xx_driver/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ /Users/zarintasmin/Mahadi/MCU1/my_workspace/stm32f4xx_driver/Driver/Inc/stm32f407xx.h
+/Users/zarintasmin/Mahadi/MCU1/my_workspace/stm32f4xx_driver/Driver/Inc/stm32f407xx.h:
